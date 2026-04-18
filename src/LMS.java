b53ac528280979af532ed0485efdd6b8d72b7bd0
Balaji N <Sathanandhbala@gmail.com>
2026-04-19 00:05:47 +0530

    LMS Menu created

---
+++ src/LMS.java
@@ -0,0 +1,16 @@
+public class LMS {
+
+    static void main(String[] args)
+    {
+        loadMenu();
+    }
+
+    public static void loadMenu()
+    {
+        System.out.println("===== Airtribe Learner Manager ====");
+        System.out.println("1.Add a Learner");
+        System.out.println("2.Display All Learners");
+        System.out.println("3.Calculate Average XP");
+        System.out.println("4.Exit");
+    }
+}
